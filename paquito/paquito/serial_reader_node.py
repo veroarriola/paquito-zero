47ffe1d6a875dcaf05730233089eca46309d9b64
veroarriola <v.arriola@ciencias.unam.mx>
2024-10-08 12:16:50 -0600

    Paréntesis faltantes

--- paquito/paquito/serial_reader_node.py
+++ paquito/paquito/serial_reader_node.py
@@ -8,7 +8,7 @@ import serial
 
 class SerialDataPublisher(Node):
     def __init__(self):
-        super.__init__('serial_data_publisher')
+        super().__init__('serial_data_publisher')
         self.publisher_ = self.create_publisher(String, 'topic', 10)
         timer_period = 0.03  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
